3c69bf3ea01a44113e947a4352a77547189af81c
Clinton Otse <otseclinton60@gmail.com>
2023-09-18 18:14:19 +0100

    Initial Setup

---
+++ getlinked/src/pages/00_Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import { Header, HomeContent, Footer } from "../components";
+import { Header } from "../components";
+
+function Home() {
+	return (
+		<>
+			<div className="app">
+				<Header />
+			</div>
+		</>
+	);
+}
+
+export default Home;

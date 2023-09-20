e00aef7da68b208c7f5dc98948222528008bacc1
Clinton Otse <otseclinton60@gmail.com>
2023-09-20 16:32:15 +0100

    Registration Complete

--- getlinked/src/components/01_Content/02_Register/01_RegistrationForm/Registration.js
+++ getlinked/src/components/01_Content/02_Register/01_RegistrationForm/Registration.js
@@ -51,96 +51,102 @@ function RegistrationContent() {
 						<div className="gl_register_form_container">
 							<form className="gl_register_form_inner">
 								<h1>Register</h1>
-								<p className="gl_part_of_move_p">Be part of this movement!</p>
-								<img
-									src={registerFormIcon}
-									alt="Part of the movement"
-									className="register_form_icon"
-								/>
-								<p className="gl_create_account_p">CREATE YOUR ACCOUNT</p>
+								<div className="gl_register_form_inner_bottom">
+									<div className="gl_movement_div">
+										<p className="gl_part_of_move_p">
+											Be part of this movement!
+										</p>
+										<img
+											src={registerFormIcon}
+											alt="Part of the movement"
+											className="register_form_icon"
+										/>
+									</div>
+									<p className="gl_create_account_p">CREATE YOUR ACCOUNT</p>
 
-								<div className="gl_register_inner_form_container">
-									{/* Left */}
-									<div className="gl_register_left_form_div">
-										<div className="gl_register_left_form_div_items">
-											<label htmlFor="teamName">Team’s Name</label>
-											<input
-												type="text"
-												name="teamName"
-												placeholder="Enter the name of your group"
-											/>
+									<div className="gl_register_inner_form_container">
+										{/* Left */}
+										<div className="gl_register_left_form_div">
+											<div className="gl_register_left_form_div_items">
+												<label htmlFor="teamName">Team’s Name</label>
+												<input
+													type="text"
+													name="teamName"
+													placeholder="Enter the name of your group"
+												/>
+											</div>
+											<div className="gl_register_left_form_div_items">
+												<label htmlFor="email">Email</label>
+												<input
+													type="email"
+													name="email"
+													placeholder="Enter your email address"
+												/>
+											</div>
+											<div className="gl_register_left_form_div_items">
+												<label htmlFor="category">Category</label>
+												<select name="category" id="category">
+													<option disabled selected hidden>
+														Select your category
+													</option>
+													<option value="design">Design</option>
+													<option value="development">Development</option>
+													<option value="marketing">Marketing</option>
+													<option value="business">Business</option>
+												</select>
+											</div>
 										</div>
-										<div className="gl_register_left_form_div_items">
-											<label htmlFor="email">Email</label>
-											<input
-												type="email"
-												name="email"
-												placeholder="Enter your email address"
-											/>
-										</div>
-										<div className="gl_register_left_form_div_items">
-											<label htmlFor="category">Category</label>
-											<select name="category" id="category">
-												<option disabled selected hidden>
-													Select your category
-												</option>
-												<option value="design">Design</option>
-												<option value="development">Development</option>
-												<option value="marketing">Marketing</option>
-												<option value="business">Business</option>
-											</select>
+
+										{/* Right */}
+										<div className="gl_register_right_form_div">
+											<div className="gl_register_right_form_div_items">
+												<label htmlFor="phone">Phone</label>
+												<input
+													type="phone"
+													name="phone"
+													placeholder="Enter your phone number"
+												/>
+											</div>
+											<div className="gl_register_right_form_div_items">
+												<label htmlFor="projectTopic">Project Topic</label>
+												<input
+													type="text"
+													name="projectTopic"
+													placeholder="What is your group project topic"
+												/>
+											</div>
+											<div className="gl_register_right_form_div_items">
+												<label htmlFor="groupSize">Group Size</label>
+												<select name="groupSize" id="groupSize">
+													<option disabled selected hidden>
+														Select
+													</option>
+													<option value="1-5">1-5</option>
+													<option value="6-10">6-10</option>
+													<option value="11-15">11-15</option>
+													<option value="16-20">16-20</option>
+												</select>
+											</div>
 										</div>
 									</div>
 
-									{/* Right */}
-									<div className="gl_register_right_form_div">
-										<div className="gl_register_right_form_div_items">
-											<label htmlFor="phone">Phone</label>
-											<input
-												type="phone"
-												name="phone"
-												placeholder="Enter your phone number"
-											/>
-										</div>
-										<div className="gl_register_right_form_div_items">
-											<label htmlFor="projectTopic">Project Topic</label>
-											<input
-												type="text"
-												name="projectTopic"
-												placeholder="What is your group project topic"
-											/>
-										</div>
-										<div className="gl_register_right_form_div_items">
-											<label htmlFor="groupSize">Group Size</label>
-											<select name="groupSize" id="groupSize">
-												<option disabled selected hidden>
-													Select
-												</option>
-												<option value="1-5">1-5</option>
-												<option value="6-10">6-10</option>
-												<option value="11-15">11-15</option>
-												<option value="16-20">16-20</option>
-											</select>
-										</div>
+									<p className="gl_review_p">
+										Please review your registration details before submitting
+									</p>
+
+									<div className="gl_agreement_div">
+										<input type="checkbox" id="agreement" name="agreement" />
+										<label htmlFor="agreement">
+											I agreed with the event terms and conditions and privacy
+											policy
+										</label>
 									</div>
-								</div>
 
-								<p className="gl_review_p">
-									Please review your registration details before submitting
-								</p>
-
-								<div className="gl_agreement_div">
-									<input type="checkbox" id="agreement" name="agreement" />
-									<label htmlFor="agreement">
-										I agreed with the event terms and conditions and privacy
-										policy
-									</label>
-								</div>
-
-								<div className="gl_register_form_submit">
-									<button type="submit" className="register_submit_btn">
-										Submit
-									</button>
+									<div className="gl_register_form_submit">
+										<button type="submit" className="register_submit_btn">
+											Register Now
+										</button>
+									</div>
 								</div>
 							</form>
 						</div>
